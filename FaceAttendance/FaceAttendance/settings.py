63946f8dd2b2481e18a4f01414e42af39b256a02
Onnetsolution <onsiplapp@gmail.com>
2025-03-05 16:29:22 +0530

    change dir

--- FaceAttendance/FaceAttendance/settings.py
+++ FaceAttendance/FaceAttendance/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'attendance', 'static')
 ]
 
 # Default primary key field type
